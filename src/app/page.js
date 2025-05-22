1da46f01afe268dc8bd8e9b14d588d523c401758
hamzacodev <hamzacodev@gmail.com>
2025-05-22 12:11:36 +0500

    ok

--- src/app/page.js
+++ src/app/page.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const page = () => {
   return (
-    <div className="bg-green-200 h-screen w-full flex items-center justify-center">
+    <div className="bg-black h-screen w-full flex items-center justify-center text-white">
       This is my site
     </div>
   );
